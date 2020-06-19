07cf53773b874d0ee449e144e877a01b1fc6c437
kelinn <kasvillarreal@gmail.com>
2020-06-18 22:05:24 -0600

    lab en parejas

--- app/src/main/java/com/example/android/roomwordssample/WordListAdapter.java
+++ app/src/main/java/com/example/android/roomwordssample/WordListAdapter.java
@@ -1,26 +1,22 @@
 package com.example.android.roomwordssample;
 
-/*
- * Copyright (C) 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Collections;
@@ -28,34 +24,108 @@ import java.util.List;
 
 
 public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
-
+    private WordViewModel mWordViewModel;
+    private ViewGroup _parent;
+    private WordListAdapter adapter;
     class WordViewHolder extends RecyclerView.ViewHolder {
         private final TextView wordItemView;
 
+        public  Button updateBTN;
+        public  Button deleteBTN;
+
+
+
         private WordViewHolder(View itemView) {
             super(itemView);
+
             wordItemView = itemView.findViewById(R.id.textView);
+            updateBTN = (Button) itemView.findViewById(R.id.editBtn);
+            deleteBTN = (Button) itemView.findViewById(R.id.deleteBtn);
+            mWordViewModel = ViewModelProviders.of((FragmentActivity) mInflater.getContext()).get(WordViewModel.class);
+
         }
     }
 
     private final LayoutInflater mInflater;
     private List<Word> mWords = Collections.emptyList(); // Cached copy of words
 
-    WordListAdapter(Context context) {
+    WordListAdapter(Context context, WordViewModel mWordViewM) {
+        mWordViewModel = mWordViewM;
         mInflater = LayoutInflater.from(context);
+        adapter = this;
     }
 
     @Override
     public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+        _parent = parent;
         return new WordViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(WordViewHolder holder, int position) {
-        Word current = mWords.get(position);
+    public void onBindViewHolder(final WordViewHolder holder, final int position) {
+
+        final Word current = mWords.get(position);
+
         holder.wordItemView.setText(current.getWord());
+
+        holder.updateBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                View _inflater = mInflater.inflate(R.layout.edit_word, _parent, false);
+
+                final EditText editText;
+
+                editText = (EditText) _inflater.findViewById(R.id.word_);
+
+                editText.setText(current.getWord());
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(mInflater.getContext());
+                dialogo1.setTitle(current.getWord());
+                dialogo1.setMessage("¿ Desea Editar el item ?");
+                dialogo1.setView(_inflater);
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Editar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        current.setmWord(editText.getText().toString());
+                        mWordViewModel.update(current);
+                        adapter.notifyItemChanged(position);
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        dialogo1.dismiss();
+                    }
+                });
+                dialogo1.show();
+
+            }
+        });
+
+        holder.deleteBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(mInflater.getContext());
+                dialogo1.setTitle(current.getWord());
+                dialogo1.setMessage("¿ Desea eliminar el item ?");
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        mWordViewModel.delete(current);
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        dialogo1.dismiss();
+                    }
+                });
+                dialogo1.show();
+            }
+        });
     }
+
+
 
     void setWords(List<Word> words) {
         mWords = words;
--- app/src/main/java/com/example/android/roomwordssample/WordRepository.java
+++ app/src/main/java/com/example/android/roomwordssample/WordRepository.java
@@ -1,58 +1,36 @@
 package com.example.android.roomwordssample;
 
-/*
- * Copyright (C) 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import android.app.Application;
 import androidx.lifecycle.LiveData;
 import android.os.AsyncTask;
 
 import java.util.List;
 
-/**
- * Abstracted Repository as promoted by the Architecture Guide.
- * https://developer.android.com/topic/libraries/architecture/guide.html
- */
-
 class WordRepository {
 
     private WordDao mWordDao;
     private LiveData<List<Word>> mAllWords;
 
-    // Note that in order to unit test the WordRepository, you have to remove the Application
-    // dependency. This adds complexity and much more code, and this sample is not about testing.
-    // See the BasicSample in the android-architecture-components repository at
-    // https://github.com/googlesamples
     WordRepository(Application application) {
         WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
         mWordDao = db.wordDao();
         mAllWords = mWordDao.getAlphabetizedWords();
     }
 
-    // Room executes all queries on a separate thread.
-    // Observed LiveData will notify the observer when the data has changed.
+
     LiveData<List<Word>> getAllWords() {
         return mAllWords;
     }
 
-    // You must call this on a non-UI thread or your app will crash.
-    // Like this, Room ensures that you're not doing any long running operations on the main
-    // thread, blocking the UI.
+
     void insert(Word word) {
         new insertAsyncTask(mWordDao).execute(word);
+    }
+    void update(Word word) {
+        new updateAsyncTask(mWordDao).execute(word);
+    }
+    void delete(Word word) {
+        new deleteAsyncTask(mWordDao).execute(word);
     }
 
     private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
@@ -66,6 +44,36 @@ class WordRepository {
         @Override
         protected Void doInBackground(final Word... params) {
             mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+    private static class updateAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        updateAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.update(params[0]);
+            return null;
+        }
+    }
+
+    private static class deleteAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        deleteAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.delete(params[0]);
             return null;
         }
     }
